4f28d9384e06293d8c86e1e14804e546bbfa2616
Umut <143949134+LinuxUsersLinuxMint@users.noreply.github.com>
2023-11-28 21:06:14 +0300

    Platform library has been added.
    Signed-off-by: Umut <143949134+LinuxUsersLinuxMint@users.noreply.github.com>

--- PythonPro.py
+++ PythonPro.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import Basic_Maths, dataclasses, signal, s2clientprotocol, time, os, ossaudiodev, _osx_support, setuptools, servicemanager, cachetools, cgi, sys, symtable, sysconfig, syslog, linecache, ldap3, lib2to3, locale, _threading_local, logging, lzma, shelve, farmhash, gdb, qrcode, openpyxl, opentracing, operator, opcode, _operator, optparse, base64, binascii, bisect, bs4, hashlib, hdbcli, jack, json, tkinter, _tkinter, tempfile, termios, deprecated, queue, quopri, csv, _csv, colorama, cProfile, abc, farmhash, flake8_2020, flake8_builtins, flake8_simplify
+import Basic_Maths, dataclasses, signal, s2clientprotocol, time, os, platform, ossaudiodev, _osx_support, setuptools, servicemanager, cachetools, cgi, sys, symtable, sysconfig, syslog, linecache, ldap3, lib2to3, locale, _threading_local, logging, lzma, shelve, farmhash, gdb, qrcode, openpyxl, opentracing, operator, opcode, _operator, optparse, base64, binascii, bisect, bs4, hashlib, hdbcli, jack, json, tkinter, _tkinter, tempfile, termios, deprecated, queue, quopri, csv, _csv, colorama, cProfile, abc, farmhash, flake8_2020, flake8_builtins, flake8_simplify
 
 def p(string):
     print("{0}", format(string))
